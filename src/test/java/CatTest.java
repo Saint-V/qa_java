7d7639a283bc210461f34200a0d0302e594cb990
Vasily Romanov <romanovve@yandex.ru>
2025-05-23 23:56:49 +0300

    добавил тесты

---
+++ src/test/java/CatTest.java
@@ -0,0 +1,40 @@
+import com.example.Cat;
+import com.example.Feline;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class CatTest {
+
+    @Mock
+    Feline feline;
+
+    @Test
+    public void getSoundTest() {
+        Cat cat = new Cat(feline);
+        String actual = cat.getSound();
+        assertEquals("Неверный звук кота!", "Мяу", actual);
+    }
+
+    @Test
+    public void getProperFoodTest() throws Exception{
+        Cat cat = new Cat(feline);
+        List<String> expectedListOfFood = List.of("Мясо");
+        Mockito.when(feline.eatMeat()).thenReturn(expectedListOfFood);
+
+        assertThat("Некорректный список с едой",
+                cat.getFood(),
+                equalTo(expectedListOfFood)
+        );
+    }
+}
